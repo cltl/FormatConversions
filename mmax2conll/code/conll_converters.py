6e03a66789a2ff10d1a27d173698540bf86c0d86
Martin van Harmelen <Martin@vanharmelen.com>
2018-06-05 17:30:59 +0200

    Write `CorefConverter.uniqueyfy`

--- mmax2conll/code/conll_converters.py
+++ mmax2conll/code/conll_converters.py
@@ -1,6 +1,9 @@
+import logging
 import itertools as it
 
 from .util import ValidationError
+
+logger = logging.getLogger(None if __name__ == '__main__' else __name__)
 
 
 class CorefConverter:
@@ -11,10 +14,44 @@ class CorefConverter:
 
     def __init__(self, sentences):
         self.sentences = sentences
-        self.word_ids = tuple(word['id'] for word in it.chain(*sentences))
+        self.word_ids = [word['id'] for word in it.chain(*sentences)]
         self.word_indices = dict(
             (ID, i) for i, ID in enumerate(self.word_ids)
         )
+
+    @staticmethod
+    def uniqueyfy(sets):
+        """
+        Make sure all spans within a refset are unique and that all refsets
+        have a unique set of spans (i.e. no other refset has exactly the same
+        set of spans).
+
+        Also discards empty reference sets
+        """
+        all_spans = set()
+        unique_sets = []
+        for refset in sets:
+            spans = frozenset(map(lambda ref: tuple(ref['span']), refset))
+            if spans not in all_spans:
+                all_spans.add(spans)
+                spans = set()
+                new_refset = []
+                for ref in refset:
+                    span = tuple(ref['span'])
+                    if span not in spans:
+                        spans.add(span)
+                        new_refset.append(ref)
+                    else:
+                        logger.debug(f"Discarding reference: {ref}")
+                if new_refset:
+                    unique_sets.append(new_refset)
+            else:
+                logger.debug(f"Discarding reference set: {refset}")
+        logger.debug(
+            f"Kept {len(all_spans)} reference sets"
+            f" with {sum(map(len, all_spans))} references"
+        )
+        return unique_sets
 
     def word_id_map_from_MMAX_sets(self, sets):
         """
@@ -32,9 +69,9 @@ class CorefConverter:
         word_id_map = {}
 
         # Randomly create a reference ID for every reference refset
-        for refID, refset in enumerate(sets):
-            # Make sure the spans are unique
-            for span in set(map(lambda ref: tuple(ref['span']), refset)):
+        for refID, refset in enumerate(self.uniqueyfy(sets)):
+            for ref in refset:
+                span = ref['span']
                 self.validate_span(span)
                 if len(span) == 1:
                     word_id_map.setdefault(span[0], []).append(
