d68c2b6b46533df90478f91d58179add806c765d
Martin van Harmelen <Martin@vanharmelen.com>
2018-07-06 17:23:22 +0200

    Add tests. Resources are taken from:
    COREA: https://ivdnt.org/downloads/taalmaterialen/tstc-corea-coreferentiecorpus-c
    SoNaR-1: https://ivdnt.org/downloads/taalmaterialen/tstc-sonar-corpus

---
+++ mmax2conll/tests/test_main.py
@@ -0,0 +1,97 @@
+import os
+import shutil
+import logging
+
+from mmax2conll.main import Main
+
+logger = logging.getLogger(None if __name__ == '__main__' else __name__)
+
+
+def test_corea_empty_dir(caplog, empty_dir, corea_config):
+    caplog.set_level(logging.DEBUG)
+    output_dir = 'output_dir'
+    try:
+        Main.main([
+            corea_config,
+            output_dir,
+            "-d",
+            empty_dir
+        ]),
+    finally:
+        if os.path.exists(output_dir):
+            shutil.rmtree(output_dir)
+
+
+def test_sonar_empty_dir(caplog, empty_dir, sonar_config):
+    caplog.set_level(logging.DEBUG)
+    output_dir = 'output_dir'
+    try:
+        Main.main([
+            sonar_config,
+            output_dir,
+            "-d",
+            empty_dir
+        ]),
+    finally:
+        if os.path.exists(output_dir):
+            shutil.rmtree(output_dir)
+
+
+def test_sonar_fill_spans(caplog, sonar_dir, sonar_fill_spans_config):
+    caplog.set_level(logging.DEBUG)
+    output_dir = 'output_dir'
+    try:
+        Main.main([
+            sonar_fill_spans_config,
+            output_dir,
+            "-d",
+            sonar_dir
+        ]),
+    finally:
+        if os.path.exists(output_dir):
+            shutil.rmtree(output_dir)
+
+
+def test_sonar_problem_only(caplog, sonar_dir, sonar_problem_only_config):
+    caplog.set_level(logging.DEBUG)
+    output_dir = 'output_dir'
+    try:
+        Main.main([
+            sonar_problem_only_config,
+            output_dir,
+            "-d",
+            sonar_dir
+        ]),
+    finally:
+        if os.path.exists(output_dir):
+            shutil.rmtree(output_dir)
+
+
+def test_corea(caplog, corea_dir, corea_config):
+    caplog.set_level(logging.DEBUG)
+    output_dir = 'output_dir'
+    try:
+        Main.main([
+            corea_config,
+            output_dir,
+            "-d",
+            corea_dir
+        ]),
+    finally:
+        if os.path.exists(output_dir):
+            shutil.rmtree(output_dir)
+
+
+def test_sonar(caplog, sonar_dir, sonar_config):
+    caplog.set_level(logging.DEBUG)
+    output_dir = 'output_dir'
+    try:
+        Main.main([
+            sonar_config,
+            output_dir,
+            "-d",
+            sonar_dir
+        ]),
+    finally:
+        if os.path.exists(output_dir):
+            shutil.rmtree(output_dir)
